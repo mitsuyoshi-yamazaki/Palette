8d117f63c0987fabf55a2b01d4a6fbfd9bf37567
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-07-09 16:12:19 +0900

    Initial Commit

---
+++ Palette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Palette
+//
+//  Created by Yamazaki Mitsuyoshi on 2019/07/09.
+//  Copyright © 2019 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

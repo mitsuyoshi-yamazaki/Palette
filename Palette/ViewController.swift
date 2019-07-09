40e60ac28945de65e46180dbe1c6d31799988436
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-07-09 16:37:58 +0900

    Write function structure

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private let refreshInterval = 0.5
 
+internal final class ViewController: UIViewController {
+
+  @IBOutlet private weak var imageView: UIImageView!
+  
+  private var image: UIImage?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    
+    image = imageView.image
   }
-
-
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    startProcessing() // FixMe: It does NOT assume that it will be called multiple times
+  }
 }
 
+private extension ViewController {
+  func startProcessing() {
+    
+    _ = Timer.scheduledTimer(withTimeInterval: refreshInterval, repeats: true, block: { [weak self] _ in
+      DispatchQueue.main.async {
+        guard let image = self?.imageView.image else { return }
+        
+        self?.imageView.image = self?.convert(image: image)
+      }
+    })
+  }
+  
+  func convert(image: UIImage) -> UIImage? {
+    guard let imageData = image.pngData() else { return nil }
+    
+    return image
+  }
+}
+
+extension Data {
+  func imageDescription() -> String {
+    return ""
+  }
+}
